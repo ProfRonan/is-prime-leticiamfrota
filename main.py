2b6425b2d294caea32808f038aa3fec1d3cf52e4
leticiamfrota <leticiafrotamesquita@alu.ufc.br>
2023-04-13 17:44:42 -0300

    Execício Concluído

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 numero = int(input())
-
-if numero <= 0:
-    print('Número inválido')
-else:
-    if numero % 2 == 0 and numero != 2:
-            print('Não primo')
-    if numero % 2 != 0 or numero == 2:
-            print('Primo')
+if numero > 2:
+    if numero % 2 == 0:
+        print('Não primo')
+    if numero % 2 != 0:
+        print('Primo')
+elif numero == 1:
+    print('Não Primo')
+elif numero == 2:
+    print('Primo')
